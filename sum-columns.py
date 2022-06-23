98efb3ac447fd3a528a3aed8245ba144f7631532
khanhbro35 <khanhbro35@gmail.com>
2022-06-23 16:35:07 +0000

    submit/Dinh_Khanh

--- sum-columns.py
+++ sum-columns.py
@@ -1,1 +1,24 @@
 # Your code
+import csv
+import sys
+
+file = sys.argv[1]
+column_num = int(sys.argv[2])
+sum=0
+list_column = []
+with open(file, 'rt') as f:
+    read = csv.reader(f)
+    header = []
+    header =  next(read)
+    if column_num <= len(header) and column_num >= 0:
+        for row in read:
+            list_column.append(row[column_num])
+        for num in list_column:
+            if num.isnumeric():
+                sum+=int(num)
+            else:
+                continue
+        print("Total is: " + str(sum))
+    else:
+        print("colum isn't exist")
+        exit(0)
